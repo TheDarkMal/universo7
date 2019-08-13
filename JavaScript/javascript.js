1c6603c95a86a18f760947eb7f990b128d48c7c2
Mauricio Vergara <mauricio301051020@gmail.com>
2019-08-13 09:55:43 -0500

    haber aqui hay ayudas :V

---
+++ JavaScript/javascript.js
@@ -0,0 +1,13 @@
+
+
+
+btn.addEventListener('click', function(){
+let  producto = document.getElementById('producto').values;
+let cant = document.getElementById('cantidad').values;
+
+if( producto==='' || cant===''){
+    alert("faltan campos por llenar")
+}
+
+});
+
